a636c5de1340534d08930df1448617d7c022ae7e
Anton Buksa <anton.buksa@infomaniak.com>
2018-10-16 17:53:03 +0200

    eyy

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
-  GITHUB_CALLBACK_URL: '',
+  GITHUB_CLIENT_ID: 'c6a2d63dfbd9bd86182a',
+  GITHUB_CLIENT_SECRET: '6479c917bfce4cfd029ee20c5caa3b56498bf4fb',
+  GITHUB_CALLBACK_URL: 'https://localhost:8080/api/user/authViaGitHub/callback',
   DBURL: 'mongodb://localhost:27017/reforum',
 };
