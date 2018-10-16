4bd81451e7dcf9de3ee2d68e97a7ead33fb9474d
Anton Buksa <anton.buksa@infomaniak.com>
2018-10-16 18:06:43 +0200

    eyy lmao

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
   GITHUB_CLIENT_ID: 'c6a2d63dfbd9bd86182a',
   GITHUB_CLIENT_SECRET: '6479c917bfce4cfd029ee20c5caa3b56498bf4fb',
-  GITHUB_CALLBACK_URL: 'https://localhost:8080/api/user/authViaGitHub/callback',
+  GITHUB_CALLBACK_URL: 'https://raze-alonsus.herokuapp.com/admin',
   DBURL: process.env.MONGODB_URI || "mongodb://localhost:27017/reforum",
 };
